2016e567c5dfffa5b7d6dc468d03ce080365c974
ak-ash93 <ash9793ak@gmail.com>
2025-04-08 21:30:55 +0800

    logout functionality completed

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      message: "Logout successful",
+      success: true,
+    });
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    return response;
+  } catch (error) {
+    const err = error as Error;
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,74 @@
 "use client";
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import axios from "axios";
 
-export default function profilePage() {
+export default function ProfilePage() {
+  const router = useRouter();
+  useEffect(() => {
+    const links = document.querySelectorAll(".hover-link");
+
+    links.forEach((link) => {
+      const underline = link.querySelector(".underline");
+
+      if (!underline) return;
+
+      gsap.set(underline, { width: "0%" });
+
+      link.addEventListener("mouseenter", () => {
+        gsap.to(underline, { width: "100%", duration: 1 });
+      });
+
+      link.addEventListener("mouseleave", () => {
+        gsap.to(underline, { width: "0%", duration: 0.3 });
+      });
+    });
+  }, []);
+
+  const logOut = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log(error);
+        toast.error(error.message); // Show error message in toast
+      } else {
+        console.log("Unexpected error", error);
+        toast.error("Something went wrong");
+      }
+    }
+  };
+
   return (
-    <div className="max-w-screen h-screen ">
-      <div className="w-[80vw] h-[8vh] translate-x-40 p-4  border-2 border-amber-50 flex justify-between items-center">
-        <div className="w-[30px] h-[30px] border-2 border-amber-50 rounded-full"></div>
-        <h2>This is a navbar</h2>
+    <div className="max-w-screen h-screen bg-white text-gray-500">
+      <div className="w-[80vw] h-[8vh] translate-x-40 p-5 flex justify-between items-center border-b-2 border-gray-300 rounded-2xl">
+        <div className="w-[80px] h-[30px] font-extrabold text-gray-400">
+          SHERwell
+        </div>
+        <div className="flex items-center gap-6">
+          {["About", "Contact", "News"].map((text) => (
+            <a
+              key={text}
+              className="hover-link relative p-4  text-lg font-semibold text-gray-500 hover:text-gray-800 cursor-pointer"
+            >
+              {text}
+              <div className="underline absolute left-0 bottom-0 h-0.5 bg-black w-0" />
+            </a>
+          ))}
+          <button
+            onClick={logOut}
+            className="bg-blue-400 px-5 py-1 text-sm font-semibold rounded-lg text-white hover:bg-red-400 cursor-pointer"
+          >
+            Logout
+          </button>
+        </div>
       </div>
 
-      <div className=" flex items-center justify-center w-full h-full">
+      <div className="flex items-center justify-center w-full h-full">
         <h1>Profile Page</h1>
       </div>
     </div>
